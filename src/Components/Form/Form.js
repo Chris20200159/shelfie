25940d5c1ca28133955aa0aba6be44003d879e29
Chris Vega <catalina@MacBook-Pro.local>
2020-08-07 16:40:35 -0700

    Phase 3.2 Add logo. CSS formatting. Header

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -10,6 +10,31 @@ class Form extends React.Component {
       imgurl: ''
     }
   }
+
+
+
+
+  render(){
+    console.log(this.state)
+    return <div className="form-container">
+      <div class="Form">
+        {/* <div class="form_img_preview" style="background-image"></div> */}
+        <p>Image URL:</p>
+        <input type="text" value/>
+        <p>Product Name:</p>
+        <input type="text" value/>
+        <p>Price:</p>
+        <input type="text" pattern="[0-9]*" value="0"/>
+      </div>
+        <div class="form-button-box">
+          <button>Cancel</button>
+          <button>Add To Inventory</button>
+        </div>
+
+    </div>
+  }
+
+
 }
 
 export default Form;
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Header.css';
+import logo from '../../logo.png';
 
 const Header = () => {
   return (
-    <div className="Header">
+    <header className="Header">
+      <img src={logo} alt="Logo"/>
+      <h1>
+        SHELFIE
+      </h1>
 
-
-    </div>
+    </header>
   );
 }
 
